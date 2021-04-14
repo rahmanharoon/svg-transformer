0afe5f805b9284d90835705a7f9d707a11a10b2b
Rahman Haroon <rahmanharoon128@gmail.com>
2021-04-14 22:11:25 +0530

    second commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import WhatsAppIcon from "../../assets/svg/whatsapp_icon.svg";
 
@@ -9,93 +9,72 @@ const MainScreen = () => {
   const [isViolet, setIsViolet] = React.useState(false);
 
   return (
-    <View
-      style={{
-        flex: 1,
-      }}
-    >
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "space-around",
-          alignItems: "center",
-        }}
-      >
-        <TouchableOpacity
-          style={{
-            flex: 1,
-            height: "100%",
-            width: "100%",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
+    <View style={Styles.container}>
+      <TouchableOpacity style={Styles.button}>
+        <WhatsAppIcon
+          style={[
+            isBlue
+              ? {
+                  color: "#0000FF",
+                }
+              : { color: "#4FCE5D" },
+          ]}
+          height={50}
+          width={50}
           onPress={() => {
             setIsBlue(!isBlue);
           }}
-        >
-          <WhatsAppIcon
-            style={[
-              isBlue
-                ? {
-                    color: "#0000FF",
-                  }
-                : { color: "#4FCE5D" },
-            ]}
-            height={50}
-            width={50}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{
-            flex: 1,
-            height: "100%",
-            width: "100%",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity style={Styles.button}>
+        <WhatsAppIcon
+          style={[
+            isRed
+              ? {
+                  color: "#FF0000",
+                }
+              : { color: "#4FCE5D" },
+          ]}
+          height={50}
+          width={50}
           onPress={() => {
             setIsRed(!isRed);
           }}
-        >
-          <WhatsAppIcon
-            style={[
-              isRed
-                ? {
-                    color: "#FF0000",
-                  }
-                : { color: "#4FCE5D" },
-            ]}
-            height={50}
-            width={50}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{
-            flex: 1,
-            height: "100%",
-            width: "100%",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity style={Styles.button}>
+        <WhatsAppIcon
+          style={[
+            isViolet
+              ? {
+                  color: "#DA70D6",
+                }
+              : { color: "#4FCE5D" },
+          ]}
+          height={50}
+          width={50}
           onPress={() => {
             setIsViolet(!isViolet);
           }}
-        >
-          <WhatsAppIcon
-            style={[
-              isViolet
-                ? {
-                    color: "#DA70D6",
-                  }
-                : { color: "#4FCE5D" },
-            ]}
-            height={50}
-            width={50}
-          />
-        </TouchableOpacity>
-      </View>
+        />
+      </TouchableOpacity>
     </View>
   );
 };
 export default MainScreen;
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  button: {
+    flex: 1,
+    height: "100%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
